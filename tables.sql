3898cc36d874a8cb74b121e2e2888f0172ac45a0
mr sebski <mrsebski@gmail.com>
2021-02-18 09:30:53 +0000

    2nd commit

---
+++ tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+
+CREATE TABLE articles (
+
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title CHAR(55) NOT NULL,
+    body VARCHAR(10000),
+    status BIT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
